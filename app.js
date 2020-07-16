529abf247e8def2932e58ff02acf7e5b66e7d686
andrealfian420 <andrealfian69699@gmail.com>
2020-07-16 16:32:13 +0700

    Fix cors error

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const app = express();
 const todoAPI = require("./routes/api/todo");
@@ -7,16 +8,17 @@ const todoAPI = require("./routes/api/todo");
 dotenv.config();
 const PORT = process.env.PORT || 3300;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/todo", todoAPI);
+
 app.get("/", (req, res) => {
   return res.status(403).json({
     status: "error",
     message: `You're forbidden to access this endpoint`,
   });
 });
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use("/api/todo", todoAPI);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
